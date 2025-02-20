0ed6fc13a47af4b889f440f9d26adb199ca77c96
SagarBhadra01 <sagarbhadra404@gmail.com>
2025-02-20 20:00:48 +0530

    Create 2_Operator.js

---
+++ JS/2_Operator.js
@@ -0,0 +1,27 @@
+//arithmatic operators
+let sum = 5 + 3; // Addition
+let diff = 10 - 2; // Subtraction
+let p = 4 * 2; // Multiplication
+let q = 8 / 2; // Division
+console.log(sum, diff, p, q);
+
+//assigned operators
+let n = 10;
+n += 5;
+n *= 2;
+console.log(n);
+
+//comparison operators
+console.log(10 > 5);
+console.log(10 === "10");
+
+//logical operators
+let a = true,
+  b = false;
+console.log(a && b); // Logical AND
+console.log(a || b); // Logical OR
+
+//bitwise operators
+let res1 = 5 & 1; // Bitwise AND
+let res2 = 5 | 1; // Bitwise or
+console.log(res1, res2);
