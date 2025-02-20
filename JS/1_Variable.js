54756cc6f17148195f68ec9d16b1e3ab0f1faedb
SagarBhadra01 <sagarbhadra404@gmail.com>
2025-02-20 20:00:44 +0530

    Create 1_Variable.js

---
+++ JS/1_Variable.js
@@ -0,0 +1,26 @@
+//The var keyword is used to declare a variable. It has a function-scoped or globally-scoped behaviour.
+var a1 = 3;
+
+//The let keyword is introduced in ES6, has block scope and cannot be re-declared in the same scope.
+let a2 = 4;
+
+//The const keyword declares variables that cannot be reassigned. It’s block-scoped as well.
+const a3 = 6;
+
+//data types-
+let n = 45; //number
+let pi = 3.14; //decimal number
+let s = "hello world"; //string
+let bool = true; //boolean
+let notAssigned; //undefine
+let empty = null; //null
+console.log(n, pi, s, bool, notAssigned, empty);
+
+let obj = {
+  name: "Amit",
+  age: 25,
+}; //object
+console.log(obj);
+
+let arr = ["red", "green", "blue"];
+console.log(arr);
