ef8a1e720983e52ef8c40cd4eedeccfaf27f13d5
SagarBhadra01 <sagarbhadra404@gmail.com>
2025-03-18 22:24:15 +0530

    Update 4_loops.js

---
+++ JS/4_loops.js
@@ -0,0 +1,17 @@
+//for loop
+for (let i = 1; i <= 3; i++) {
+  console.log("Count:", i);
+}
+
+//while loop
+let i = 0;
+while (i < 3) {
+  console.log("Number:", i);
+  i++;
+}
+
+//dowhile loop
+do {
+  console.log("Iteration:", i);
+  i++;
+} while (i < 3);
